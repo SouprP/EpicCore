8f03671930f2e450cf29e547a25f82a1bc3d61c3
SouprP <potyrala.igor@gmail.com>
2022-08-27 15:01:18 +0200

    Initial commit

---
+++ src/main/java/me/souprpk/epiccore/EpicCore.java
@@ -0,0 +1,17 @@
+package me.souprpk.epiccore;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class EpicCore extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
